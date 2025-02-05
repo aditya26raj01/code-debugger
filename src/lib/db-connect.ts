b11b0da92d0d936fb3ebea2912dc4a2c5db0b276
Aditya Raj <aditya26raj01@gmail.com>
2025-02-05 22:30:32 +0530

    minor errors

--- src/lib/db-connect.ts
+++ src/lib/db-connect.ts
@@ -3,13 +3,27 @@ import mongoose from "mongoose";
 const MONGO_URI = process.env.MONGO_URI;
 
 if (!MONGO_URI) {
-  throw new Error("Please define the MONGO_URI environment variable inside .env.local");
+  throw new Error(
+    "Please define the MONGO_URI environment variable inside .env.local"
+  );
 }
 
-let cached = global.mongoose;
+interface MongooseCache {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
+}
+declare global {
+  let mongoose: MongooseCache;
+}
+
+const globalWithMongoose = global as typeof globalThis & {
+  mongoose: MongooseCache;
+};
+
+let cached = globalWithMongoose.mongoose;
 
 if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
+  cached = globalWithMongoose.mongoose = { conn: null, promise: null };
 }
 
 async function dbConnect() {
@@ -19,7 +33,7 @@ async function dbConnect() {
 
   if (!cached.promise) {
     cached.promise = mongoose
-      .connect(MONGO_URI, {
+      .connect(MONGO_URI!, {
         dbName: "sand-bugger",
       })
       .then((mongoose) => mongoose);
